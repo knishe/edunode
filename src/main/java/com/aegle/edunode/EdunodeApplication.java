ad8aa269884cef9b1522adba4f7d6a08ed8baf15
knishe <knishe@gmail.com>
2017-05-11 13:40:31 +0800

    Initial commit

---
+++ src/main/java/com/aegle/edunode/EdunodeApplication.java
@@ -0,0 +1,12 @@
+package com.aegle.edunode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdunodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdunodeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aegle/edunode/EdunodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aegle.edunode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EdunodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
